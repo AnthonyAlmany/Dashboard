3a3721e8426469ce9f78de8eb40aec2c881ac47b
AnthonyAlm <anthony.almany@gmail.com>
2023-01-09 21:11:19 +1100

    Add Router logic

--- src/App.tsx
+++ src/App.tsx
@@ -19,6 +19,17 @@ function App() {
   return (
     <div className="App">
       <h1>Hello</h1>
+
+      <Link style={{ marginRight: "5px" }} to="/">Home</Link>
+
+      {buttonsList.map((btn: buttonListType) => <Link style={{ margin: "5px" }} key={btn.name} to={`/${btn.name}`}>{btn.logo}</Link>)}
+
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/weather" element={<Weather />} />
+        <Route path="/crypto" element={<Crypto />} />
+      </Routes>
     </div>
   );
 }
