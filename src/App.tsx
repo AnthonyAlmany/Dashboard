f02a44efeed6d980a32debd8c7634888dfc1441e
AnthonyAlm <anthony.almany@gmail.com>
2023-01-09 21:04:15 +1100

    Add router logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,37 @@
 import React from 'react';
-
+import {
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
+import Weather from "./components/Weather"
+import Home from "./components/Home"
+import Crypto from './components/Crypto';
+import buttonsList from "./datas/buttonsList"
 
 function App() {
 
-  
+  type buttonListType = {
+    name: string;
+    logo: string;
+  }
+
   return (
-    <div className="App">
-     <h1>Hello</h1>
+    <div className="App" >
+      <h1>Dashboard</h1>
+
+      <Link style={{ marginRight: "5px" }} to="/">Home</Link>
+
+      {buttonsList.map((btn: buttonListType) => <Link style={{ margin: "5px" }} key={btn.name} to={`/${btn.name}`}>{btn.logo}</Link>)}
+
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/weather" element={<Weather />} />
+        <Route path="/crypto" element={<Crypto />} />
+      </Routes>
+
+
     </div>
   );
 }
---
+++ src/datas/buttonsList.tsx
@@ -0,0 +1,12 @@
+const buttonsList = [
+    {
+        name: "Weather",
+        logo: "logoWeather"
+    },
+    {
+        name: "Crypto",
+        logo: "logoCrypto"
+    },
+]
+
+export default buttonsList
