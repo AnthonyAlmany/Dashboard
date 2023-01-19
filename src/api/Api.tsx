5df4eeae8265d539d1025b1bc86011b9904790b4
AnthonyAlm <anthony.almany@gmail.com>
2023-01-19 19:27:29 +1100

    working on api

---
+++ src/api/Api.tsx
@@ -0,0 +1,198 @@
+import React, { useEffect, useState } from 'react'
+import axios from 'axios';
+
+// import City from './City';
+// interface foo {
+//     city: any
+// }
+// interface bar {
+//     data: any
+
+// }
+interface valueType {
+    config: any
+    data: any
+    headers: any
+    request: any
+    status: any
+    statusText: any
+}
+
+interface marketType {
+    value: valueType
+    status: string
+
+}
+
+function Api() {
+    const [weatherData, setWeatherData] = useState<any[]>([])
+    const cities: string[] = ['Paris', 'London', 'Sydney']
+    const coins = ['bitcoin', 'ripple']
+    const [marketData, setMarketData] = useState<marketType[]>()
+
+    // useEffect(() => {
+    //     const fetchCrypto = () => {
+    //         const getUrl = Promise.allSettled(coins.map(coin => axios.get(`https://api.coingecko.com/api/v3/simple/price?ids=${coin}&vs_currencies=usd`)))
+    //         getUrl.then((responses) => {
+    //             // const data = res.data;
+    //             responses.map(result => {
+    //                 const data: any = result.status === 'fulfilled' && result.value
+    //                 setMarketData(data)
+    //                 console.log(data)
+    //             })
+    //         }).catch(error => console.log(error));
+    //     }
+    //     fetchCrypto();
+    // }, [])
+
+    useEffect(() => {
+        const fetchWeather = () => {
+
+            Promise.allSettled(cities.map((city: string) => axios.get(`https://api.weatherapi.com/v1/current.json?key=${process.env.REACT_APP_WEATHER_API_KEY}&q=${city}&aqi=no`)))
+                .then(responses => {
+                    setWeatherData(responses.map(result => {
+                        const data: any = result.status === 'fulfilled' && result.value
+                        return {
+                            temperature: data.data.current.temp_c
+                        }
+                    }
+                    ));
+                })
+                .catch(console.error)
+        }
+        fetchWeather();
+    }, [])
+    console.log(weatherData)
+
+
+
+    return (
+        <div>
+            {weatherData ? weatherData.map((weather: any, i: number) => <p key={i}>{weather.temperature}</p>) : null}
+        </div>
+    )
+}
+
+export default Api;
+
+
+
+
+
+
+
+
+
+
+// useEffect(() => {
+//     async function fetchWeatherData() {
+
+//         const promises = cities.map((city) => axios.get(`https://api.weatherapi.com/v1/current.json?key=${process.env.REACT_APP_WEATHER_API_KEY}&q=${city}&aqi=no`));
+//         try {
+//             const weathers = (await Promise.allSettled(promises)).map((result) => {
+//                 return {
+//                     city: result.value.data.current.temp_c
+//                 };
+//             });
+
+//             setWeatherData(weathers)
+//             console.log(weathers)
+
+//         } catch (err) {
+//             console.error(err);
+
+//         }
+//     }
+//     fetchWeatherData();
+// }, [])
+
+
+
+
+
+// useEffect(() => {
+//     const fetchWeather = () => {
+//         Promise.allSettled(cities.map(city => axios.get(`https://api.weatherapi.com/v1/current.json?key=${process.env.REACT_APP_WEATHER_API_KEY}&q=${city}&aqi=no`)).concat(axios.get())
+//             .then(responses => {
+//                 setWeatherData(responses.map(result => {
+//                     return {
+//                         city: result.value.data.current.temp_c
+//                     };
+//                 }));
+//             }, (rejected) => console.error(rejected))
+//             .catch(console.error)
+//             .finally(
+//                 console.debug('clean up generic')
+//             );
+//     }
+//     fetchWeather();
+// }, [])
+
+// console.log(weatherData)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//     const fetchCrypto = async () => {
+//         await axios.get(`https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd`)
+//             .then((res) => {
+//                 const data: number = res.data.bitcoin.usd;
+//                 console.log(data)
+//             }).catch(error => console.log(error));
+//     }
+//     fetchCrypto();
+
+
+//     const fetchMovies = async () => {
+//         await axios.get(`https://api.themoviedb.org/3/trending/all/day?api_key=${process.env.REACT_APP_MOVIES_API_KEY}`)
+//             .then((res) => {
+//                 const data: string = res.data.results[0].title;
+//                 console.log(data)
+//             }).catch(error => console.log(error));
+//     }
+//     fetchMovies();
+
+
+
+// useEffect(() => {
+//     const fetchWeather = async () => {
+//         const filterSuccessfulOnly = responses => Promise.resolve(responses.filter(({ status }) => status !== 'rejected'));
+//         const formatData = responses => Promise.resolve(responses.map(result => {
+//             return {
+//                 city: result.value.data.current.temp_c
+//             }
+//         }));
+
+//         const getResponses = Promise.allSettled(cities.map(city => axios.get(`https://api.weatherapi.com/v1/current.json?key=${process.env.REACT_APP_WEATHER_API_KEY}&q=${city}&aqi=no`)));
+//         getResponses
+//             .then(filterSuccessfulOnly)
+//             .then(formatData)
+//             .then(setWeatherData)
+//             .catch(e => console.error(e))
+
+//         console.log('Fetch other stuff and render it');
+//     }
+//     fetchWeather();
+// }, []) */}
