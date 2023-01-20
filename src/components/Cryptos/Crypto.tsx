8cd238905e233ff096ea60c81ff45b5ac63a6f72
Thomas FAISCA <faisca.thomas@gmail.com>
2023-01-20 09:39:27 +0100

    ✨ Cryptos_Panel: Add CryptoCard

--- src/components/Cryptos/Crypto.tsx
+++ src/components/Cryptos/Crypto.tsx
@@ -3,6 +3,8 @@ import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
 import Autocomplete from "@mui/material/Autocomplete";
 import * as React from "react";
+import styled from "styled-components";
+import { theme } from "../../theme/theme";
 
 export type CryptoType = {
    ath: number;
@@ -86,17 +88,54 @@ function Crypto() {
                   <TextField
                      {...params}
                      label="Choose a crypto"
-                     // inputProps={{
-                     //    ...params.inputProps,
-                     //    autoComplete: "new-password", // disable autocomplete and autofill
-                     // }}
+                     inputProps={{
+                        ...params.inputProps,
+                        autoComplete: "new-password", // disable autocomplete and autofill
+                     }}
                   />
                )}
             />
-            <p>{value?.name}</p>
+            <CryptoCardStyled>
+               <img src={value?.image} alt="crypto-logo" />
+               <h3>{value?.name.toUpperCase()}</h3>
+               {/* <h5>Current price :</h5>
+               <p>{value?.current_price} $</p> */}
+               <button>Add to favorite</button>
+            </CryptoCardStyled>
          </div>
       );
    } else return null;
 }
+
+const CryptoCardStyled = styled.div`
+   width: 200px;
+   height: 250px;
+   display: flex;
+   flex-direction: column;
+   align-items: center;
+   justify-content: space-around;
+   color: ${theme.colors.tertiary};
+   border-radius: ${theme.borderRadius.extraRounded};
+   background-color: ${theme.colors.primary};
+   box-shadow: 5px 5px 8px ${theme.colors.tertiary};
+   img {
+      height: 50px;
+      width: 50px;
+      border-radius: 50%;
+   }
+   button {
+      height: 50px;
+      border: none;
+      color: ${theme.colors.primary};
+      background-color: ${theme.colors.secondary};
+      border-radius: ${theme.borderRadius.rounded};
+      &:hover {
+         border: 2px solid ${theme.colors.secondary};
+         color: ${theme.colors.secondary};
+         background-color: ${theme.colors.primary};
+         box-shadow: 3px 3px 5px ${theme.colors.secondary};
+      }
+   }
+`;
 
 export default Crypto;
