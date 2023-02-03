fb16bdc69bd2f5c0aeef0a3f719cf601f930b6f5
AnthonyAlm <anthony.almany@gmail.com>
2023-02-03 15:56:03 +1100

    Add slider in Movies

--- src/components/Movies/Movies.tsx
+++ src/components/Movies/Movies.tsx
@@ -1,9 +1,14 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { useFetch } from "../../hooks/useFetch";
 import styled from "styled-components";
 import MovieCard from "./MovieCard";
 import { MovieResponse, MovieType } from "../../types/types";
 import { theme } from "../../theme/theme";
+
+import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
+import ArrowBackIosNewIcon from '@mui/icons-material/ArrowBackIosNew';
+
+
 
 function Movies({ handleMovie }: any): any {
    const upcomingMovies: MovieResponse | null = useFetch(
@@ -16,24 +21,60 @@ function Movies({ handleMovie }: any): any {
       "movie"
    );
 
+   const listRef: any = useRef()
+   const [slideNumber, setSlideNumber] = useState(0);
+   const [distance, setDistance] = useState<number>(0)
+
+   useEffect(() => {
+      if (listRef.current) {
+         listRef.current.style.transform = `translateX(${distance}px)`;
+      }
+
+   }, [distance])
+
+   const handleClick = (direction: any) => {
+
+      if (direction === "left" && slideNumber > 0) {
+         setSlideNumber(slideNumber - 1);
+         setDistance(prev => prev + 288)
+      }
+      if (direction === "right" && slideNumber < 15) {
+         setSlideNumber(slideNumber + 1);
+         setDistance(prev => prev - 288)
+      }
+   };
+
    return (
       <MoviesPanelStyled>
          <h1>Now playing :</h1>
+
          {nowPlayingMovies?.isLoading ? (
             <h1>Fetching datas...</h1>
          ) : (
-            <div className="movies-card-wrapper">
-               {nowPlayingMovies?.movieDatas?.map((movie: MovieType) => {
-                  return (
-                     <MovieCard
-                        key={movie.id}
-                        movie={movie}
-                        handleMovie={handleMovie}
-                     />
-                  );
-               })}
+
+            <div className="wrapper">
+               <ArrowBackIosNewIcon className="arrow arrow-left" onClick={() => handleClick("left")} />
+               <div className="movies-card-wrapper" ref={listRef}>
+
+                  {nowPlayingMovies?.movieDatas?.map((movie: MovieType) => {
+                     return (
+                        <MovieCard
+                           key={movie.id}
+                           movie={movie}
+                           handleMovie={handleMovie}
+                        />
+                     );
+                  })}
+
+               </div>
+               <ArrowForwardIosIcon className="arrow arrow-right" onClick={() => handleClick("right")} />
             </div>
+
+
+
+
          )}
+
          <h1>Upcoming :</h1>
          {upcomingMovies?.isLoading ? (
             <h1>Fetching datas...</h1>
@@ -63,36 +104,52 @@ const MoviesPanelStyled = styled.div`
    flex-direction: column;
    row-gap: 25px;
    padding: 25px 0 50px 0;
-   align-items: center;
    overflow: hidden;
+   
    h1 {
       color: ${theme.colors.secondary};
       text-decoration: underline;
    }
+
+   .wrapper{
+      position: relative;
+   }
+
    .movies-card-wrapper {
       display: flex;
-      width: 99%;
-      overflow-x: scroll;
+      width: max-content;
+      transition: all 0.6s ease;
       column-gap: 40px;
       padding: 40px 20px;
-      //white-space: nowrap;
-      /*column-gap: 35px;
-      width: 100%;
-      padding: 20px;
-      overflow-x: scroll;
-      white-space: normal; */
-      /* display: grid;
-      grid-template-columns: repeat(3, 1fr);
-      gap: 35px; */
-      &::-webkit-scrollbar {
-         background: ${theme.colors.xtraLightSecondary};
-         border-radius: 2rem;
-         }
+      // &::-webkit-scrollbar {
+      //    background: ${theme.colors.xtraLightSecondary};
+      //    border-radius: 2rem;
+      //    }
      
-         &::-webkit-scrollbar-thumb {
-           background: ${theme.colors.secondary};
-           border-radius: 2rem;
-         }
+      //    &::-webkit-scrollbar-thumb {
+      //      background: ${theme.colors.secondary};
+      //      border-radius: 2rem;
+      //    }
+   }
+   
+   .arrow{
+      position: absolute;
+      z-index: 90;
+      background-color: grey;
+      opacity: 0.6;
+      height: 4rem;
+      width: 60px;
+      top: 18rem;
+      &:hover{
+         opacity: 0.9;
+         cursor: pointer;
+      }
+   }
+   .arrow-left{
+      left: 2px;
+   }
+   .arrow-right{
+      right: 2px;
    }
 `;
 
